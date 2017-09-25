4879572f80f30ece893df52907a8248c0700e88a
Jacob Foster <jacob.foster@stonesoup.solutions>
2017-09-25 16:58:14 -0700

    Initial commit

---
+++ ex5.py
@@ -0,0 +1,14 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 * 2.54 # cm
+weight = 180 * 0.453592 # kgs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} centimeters tall.")
+print(f"He's {weight} kilograms heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
